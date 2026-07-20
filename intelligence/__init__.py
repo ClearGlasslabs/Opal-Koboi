28291b8cb796decf56940974e05f03394a4515c9
Desmond Otieno <77194670+ClearGlasslabs@users.noreply.github.com>
2026-07-20 01:01:06 -0400

    Export Financial Watchdog entity models

--- intelligence/__init__.py
+++ intelligence/__init__.py
@@ -1,13 +1,31 @@
 """ClearGlass intelligence components."""
 
 from .entity_extractor import (
+    AnyInfluenceEntity,
+    ContractEntity,
+    DateEntity,
+    ExtractedEntity,
+    ExtractionPayload,
     ExtractionResult,
+    FinancialAmountEntity,
     LLMEntityExtractor,
+    OrganizationEntity,
+    PersonEntity,
+    PolicyEntity,
     enrich_evidence_with_llm_entities,
 )
 
 __all__ = [
+    "AnyInfluenceEntity",
+    "ContractEntity",
+    "DateEntity",
+    "ExtractedEntity",
+    "ExtractionPayload",
     "ExtractionResult",
+    "FinancialAmountEntity",
     "LLMEntityExtractor",
+    "OrganizationEntity",
+    "PersonEntity",
+    "PolicyEntity",
     "enrich_evidence_with_llm_entities",
 ]
